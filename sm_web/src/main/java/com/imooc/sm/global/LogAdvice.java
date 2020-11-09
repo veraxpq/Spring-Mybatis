b46cd07e029bfa0b61ba26d1a08c2e003d9e5d0d
veraxpq <634505024@qq.com>
2020-11-09 20:28:13 +0800

    sm

---
+++ sm_web/src/main/java/com/imooc/sm/global/LogAdvice.java
@@ -0,0 +1,83 @@
+package com.imooc.sm.global;
+
+import com.imooc.sm.entity.Log;
+import com.imooc.sm.entity.Staff;
+import com.imooc.sm.service.LogService;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Component
+@Aspect
+public class LogAdvice {
+
+    @Autowired
+    private LogService logService;
+
+    @After("execution(* com.imooc.sm.controller..*.*(..)) && execution(* com.imooc.sm.controller.SelfController.*(..))&& execution(* com.imooc.sm.controller.*.to*(..))")
+    public void operationLog(JoinPoint joinPoint) {
+        Log log = new Log();
+        log.setMoudle(joinPoint.getTarget().getClass().getSimpleName());
+        log.setOperation(joinPoint.getSignature().getName());
+
+        HttpServletRequest request = (HttpServletRequest)joinPoint.getArgs()[0];
+        HttpSession session = request.getSession();
+        Object obj = session.getAttribute("USER");
+        Staff staff = (Staff)obj;
+        log.setOperator(staff.getAccount());
+        log.setResult("succeed");
+        logService.addOperationLog(log);
+    }
+
+    @AfterThrowing(throwing="e", pointcut="execution(* com.imooc.sm.controller..*.*(..)) && execution(* com.imooc.sm.controller.SelfController.*(..))")
+    public void systemLog(JoinPoint joinPoint, Throwable e) {
+        Log log = new Log();
+        log.setMoudle(joinPoint.getTarget().getClass().getSimpleName());
+        log.setOperation(joinPoint.getSignature().getName());
+
+        HttpServletRequest request = (HttpServletRequest)joinPoint.getArgs()[0];
+        HttpSession session = request.getSession();
+        Object obj = session.getAttribute("USER");
+        Staff staff = (Staff)obj;
+        log.setOperator(staff.getAccount());
+        log.setResult(e.getClass().getSimpleName());
+        logService.addSystemLog(log);
+    }
+
+    @After("execution(* com.imooc.sm.controller.SelfController.login(..))")
+    public void loginLog(JoinPoint joinPoint) {
+        log(joinPoint);
+    }
+
+    @Before("execution(* com.imooc.sm.controller.SelfController.logout(..))")
+    public void logoutLog(JoinPoint joinPoint) {
+        log(joinPoint);
+    }
+
+    private void log(JoinPoint joinPoint) {
+        Log log = new Log();
+        log.setMoudle(joinPoint.getTarget().getClass().getSimpleName());
+        log.setOperation(joinPoint.getSignature().getName());
+
+        HttpServletRequest request = (HttpServletRequest)joinPoint.getArgs()[0];
+        HttpSession session = request.getSession();
+        Object obj = session.getAttribute("USER");
+        if (obj==null) {
+            log.setOperator(request.getParameter("account"));
+            log.setResult("fail");
+        } else {
+            Staff staff = (Staff)obj;
+            log.setOperator(staff.getAccount());
+            log.setResult("succeed");
+        }
+        logService.addSystemLog(log);
+    }
+}
